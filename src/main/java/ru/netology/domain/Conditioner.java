723c54a3c3941050375248dd4c93a5f12c3f2808
Irina-Bebenina <irs.snoopy@gmail.com>
2020-09-05 13:09:17 +0300

    first commit

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,57 @@
+package ru.netology.domain;
+
+public class Conditioner {
+    private int currentTemperature = 22;
+
+    private boolean on;
+
+
+
+    public void increaseCurrentTemperature() {
+
+        int maxTemperature = 32;
+
+        if (currentTemperature == maxTemperature) {
+
+            return;
+
+        } else {
+
+            currentTemperature += 1;
+
+        }
+
+    }
+
+
+
+    public void decreaseCurrentTemperature() {
+
+        int minTemperature = 18;
+
+        if (currentTemperature == minTemperature) {
+
+            return;
+
+        } else {
+
+            currentTemperature -= 1;
+
+        }
+
+    }
+
+    public int getCurrentTemperature() {
+
+        return currentTemperature;
+
+    }
+
+
+    public void setCurrentTemperature(int currentTemperature) {
+
+        this.currentTemperature = currentTemperature;
+
+    }
+
+}
---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,68 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+
+    @Test
+
+    void shouldIncreaseCurrentTemperatureInNormalCondition() {
+
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.increaseCurrentTemperature();
+
+        assertEquals(23, conditioner.getCurrentTemperature());
+
+    }
+
+
+    @Test
+
+    void shouldIncreaseCurrentTemperatureInHeatCondition() {
+
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setCurrentTemperature(32);
+
+        conditioner.increaseCurrentTemperature();
+
+        assertEquals(32, conditioner.getCurrentTemperature());
+
+    }
+
+
+
+    @Test
+
+    void shouldDecreaseCurrentTemperatureInUsualConditions() {
+
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.decreaseCurrentTemperature();
+
+        assertEquals(21, conditioner.getCurrentTemperature());
+
+    }
+
+
+
+    @Test
+
+    void shouldDecreaseCurrentTemperatureInColdConditions() {
+
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setCurrentTemperature(18);
+
+        conditioner.decreaseCurrentTemperature();
+
+        assertEquals(18, conditioner.getCurrentTemperature());
+
+    }
+
+}
+
